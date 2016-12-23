b4762444ab3881b46e141d141fb2ea6b59863321
WeRockStar <werockstar@Kotchaphans-MacBook-Pro.local>
2016-12-23 12:17:10 +0700

    Add toolbar

--- app/src/main/java/xyz/guutong/androidpdfviewer/PdfViewActivity.java
+++ app/src/main/java/xyz/guutong/androidpdfviewer/PdfViewActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.github.barteksc.pdfviewer.PDFView;
 import com.github.barteksc.pdfviewer.listener.OnLoadCompleteListener;
@@ -16,19 +17,23 @@ import xyz.guutong.androidpdfviewer.Utils.DownloadFile;
 import xyz.guutong.androidpdfviewer.Utils.DownloadFileUrlConnectionImpl;
 import xyz.guutong.androidpdfviewer.Utils.FileUtil;
 
-public class PdfViewActivity extends AppCompatActivity implements DownloadFile.Listener ,OnPageChangeListener, OnLoadCompleteListener {
+public class PdfViewActivity extends AppCompatActivity implements DownloadFile.Listener, OnPageChangeListener, OnLoadCompleteListener {
 
     private PDFView pdfView;
     private String pdfUrl = "http://www.axmag.com/download/pdfurl-guide.pdf";
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        Intent intent = this.getIntent();
-        String inPdfUrl = intent.getStringExtra("pdfUrl")==null?pdfUrl:intent.getStringExtra("pdfUrl");
         setContentView(R.layout.activity_pdf_view);
+
         pdfView = (PDFView) findViewById(R.id.pdfView);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Intent intent = getIntent();
+        String inPdfUrl = intent.getStringExtra("pdfUrl") == null ? pdfUrl : intent.getStringExtra("pdfUrl");
         DownloadFile downloadFile = new DownloadFileUrlConnectionImpl(this, new Handler(), this);
         downloadFile.download(inPdfUrl, new File(this.getCacheDir(), FileUtil.extractFileNameFromURL(inPdfUrl)).getAbsolutePath());
     }
