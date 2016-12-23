dadb78c47367f87e3c339fb312f3a21c345695dc
WeRockStar <kotchaphan.m@gmail.com>
2016-12-23 14:57:04 +0700

    Add handle menu event

--- app/src/main/java/xyz/guutong/androidpdfviewer/PdfViewActivity.java
+++ app/src/main/java/xyz/guutong/androidpdfviewer/PdfViewActivity.java
@@ -1,7 +1,13 @@
 package xyz.guutong.androidpdfviewer;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Handler;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -26,21 +32,24 @@ public class PdfViewActivity extends AppCompatActivity implements DownloadFile.L
     /**
      * Default PDF url
      */
-    private String pdfUrl = "http://www.axmag.com/download/pdfurl-guide.pdf";
+    private final String pdfUrlDefault = "http://www.axmag.com/download/pdfurl-guide.pdf";
     private Toolbar toolbar;
     private PDFView pdfView;
+    private Intent intentUrl;
+    private String pdfUrl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pdf_view);
 
+        intentUrl = getIntent();
+        pdfUrl = intentUrl.getStringExtra(EXTRA_PDF_URL) == null ? this.pdfUrlDefault : intentUrl.getStringExtra(EXTRA_PDF_URL);
+
         pdfView = (PDFView) findViewById(R.id.pdfView);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        Intent intent = getIntent();
-        String pdfUrl = intent.getStringExtra(EXTRA_PDF_URL) == null ? this.pdfUrl : intent.getStringExtra(EXTRA_PDF_URL);
         downloadPdf(pdfUrl);
     }
 
@@ -63,10 +72,31 @@ public class PdfViewActivity extends AppCompatActivity implements DownloadFile.L
                 finish();
                 break;
             case R.id.menu_share:
-                
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                CharSequence[] itemsAlert = {"Copy link", "Open browser"};
+
+                builder.setItems(itemsAlert, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        final int COPY_LINK = 0;
+                        final String label = "URL";
+
+                        if (which == COPY_LINK) {
+                            ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                            ClipData clip = ClipData.newPlainText(label, pdfUrl);
+                            clipboard.setPrimaryClip(clip);
+                            return;
+                        }
+
+                        Intent intentBrowser = new Intent(Intent.ACTION_VIEW);
+                        intentBrowser.setData(Uri.parse(pdfUrl));
+                        startActivity(intentBrowser);
+                    }
+                });
+                builder.show();
                 break;
         }
-        return super.onOptionsItemSelected(item);
+        return true;
     }
 
     @Override
